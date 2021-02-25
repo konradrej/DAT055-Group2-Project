40a8f9725acef22f4e59929f73728dbf83592bdf
Konrad Rej <31326411+konradrej@users.noreply.github.com>
2021-02-25 15:52:24 +0100

    Changed FindBookingPane

--- Maven Project/client/src/main/java/FindBookingPane.java
+++ Maven Project/client/src/main/java/FindBookingPane.java
@@ -1,5 +1,6 @@
 import ObserverPattern.IObserver;
 import cinemaObjects.Booking;
+import cinemaObjects.Customer;
 import cinemaObjects.Row;
 import cinemaObjects.Seat;
 import server.GetBookingsByPhoneNumberCommand;
@@ -17,7 +18,7 @@ public class FindBookingPane extends AbstractPane implements IObserver<ClientMod
     private String ssn;
     private String phonenumber;
     private ClientModel cm;
-    private Collection<Booking> bookings;
+    private Collection<Booking> bookings = new ArrayList<>();
 
     public FindBookingPane(JFrame frame){
         super(frame);
@@ -82,69 +83,88 @@ public class FindBookingPane extends AbstractPane implements IObserver<ClientMod
     public void updateShowBookings() {
 
         Container con = (Container) contentPane.getComponent(1);
+        con.setLayout(new GridLayout());
         con.removeAll();
 
-        if (bookings != null) {
 
+        // TEMP
+        Seat newSeat = new Seat(1);
+        Collection<Seat> seats = new ArrayList<>();
+        seats.add(newSeat);
+        Row newRow = new Row(1, seats);
+        Collection<Row> rows = new ArrayList<>();
+        rows.add(newRow);
+        Booking newBooking = new Booking(cm.getShowCollection().getAllShows().iterator().next(), new Customer("Konrad", "999999", "99"), rows);
+        bookings.add(newBooking);
+        // END TEMP
+
+        if (bookings != null && bookings.size() > 0) {
             JPanel wrapperpanel = new JPanel();
-            JScrollPane scrollpane = new JScrollPane();
+            JScrollPane scrollpane = new JScrollPane(wrapperpanel);
+            scrollpane.setPreferredSize(new Dimension(200, 200));
+            scrollpane.setLayout(new ScrollPaneLayout());
 
-            wrapperpanel.setLayout(new GridLayout(-1,1));
-
-
-
+            wrapperpanel.setLayout(new BoxLayout(wrapperpanel, BoxLayout.Y_AXIS));
 
             for(Booking booking : bookings) {
                 JPanel bookingPanel = new JPanel();
                 bookingPanel.setLayout(new BorderLayout());
+                bookingPanel.setBorder(BorderFactory.createEtchedBorder());
 
                 JPanel buttonContainer = new JPanel();
                 JPanel infoContainer = new JPanel();
-
-                buttonContainer.setBorder(BorderFactory.createEtchedBorder());
-                infoContainer.setBorder(BorderFactory.createEtchedBorder());
+                infoContainer.setLayout(new BoxLayout(infoContainer, BoxLayout.Y_AXIS));
 
                 JLabel movieLabel = new JLabel("Movie: " + booking.getShow().getMovie().getTitle() );
+                movieLabel.setAlignmentX(JPanel.LEFT_ALIGNMENT);
                 infoContainer.add(movieLabel);
 
                 JLabel dateTimeLabel = new JLabel("Date and time: " + booking.getShow().getShowDateAndTime() );
+                dateTimeLabel.setAlignmentX(JPanel.LEFT_ALIGNMENT);
                 infoContainer.add(dateTimeLabel);
 
-                JLabel cinemaLabel = new JLabel("Cinema " + booking.getShow().getCinema());
+                JLabel cinemaLabel = new JLabel("Cinema " + booking.getShow().getCinema().getCinemaName());
+                cinemaLabel.setAlignmentX(JPanel.LEFT_ALIGNMENT);
                 infoContainer.add(cinemaLabel);
 
-                JLabel theaterLabel = new JLabel("Theater: " + booking.getShow().getTheater());
+                JLabel theaterLabel = new JLabel("Theater: " + booking.getShow().getTheater().getTheaterNumber());
+                theaterLabel.setAlignmentX(JPanel.LEFT_ALIGNMENT);
                 infoContainer.add(theaterLabel);
 
-                JLabel bookedRowLabel = new JLabel("Row: " + booking.getBookedRows());
-                infoContainer.add(bookedRowLabel);
 
+                JPanel rowSeatPanel = new JPanel();
+                rowSeatPanel.setAlignmentX(JPanel.LEFT_ALIGNMENT);
+                JLabel bookedSeatsLabel = new JLabel("Booked Seats: ");
+                JComboBox<String> seatsText = new JComboBox<>();
                 for(Row row: booking.getBookedRows()){
-                    Collection<Seat> bookedSeats =  booking.getBookedSeats(row.getRowNumber());
+                    for(Seat seat : booking.getBookedSeats(row.getRowNumber())){
+                        seatsText.addItem("R:" + row.getRowNumber() + " S:" + seat.getSeatNumber());
+                    }
                 }
+                rowSeatPanel.add(bookedSeatsLabel);
+                rowSeatPanel.add(seatsText);
+
+                infoContainer.add(rowSeatPanel);
 
 
-                JLabel bookedSeatsLabel = new JLabel("Seat: " + booking);
+                buttonContainer.setBackground(Color.BLACK);
+                infoContainer.setBackground(Color.RED);
+                rowSeatPanel.setBackground(Color.yellow);
 
                 bookingPanel.add(buttonContainer,BorderLayout.EAST);
                 bookingPanel.add(infoContainer, BorderLayout.CENTER);
-
+                bookingPanel.setPreferredSize(new Dimension(scrollpane.getWidth(), 200));
                 wrapperpanel.add(bookingPanel);
             }
 
             con.add(scrollpane);
-            scrollpane.add(wrapperpanel);
-
-            JLabel bookinglabel = new JLabel();
-            wrapperpanel.add(bookinglabel);
-        }
-
-        else{
+            con.setLayout(new GridLayout());
+        }else{
             JLabel label = new JLabel("No booking found");
             con.add(label);
-
-
         }
+
+        contentPane.validate();
     }
 
     public Container backToMain(){
@@ -168,7 +188,6 @@ public class FindBookingPane extends AbstractPane implements IObserver<ClientMod
             this.bookings = bookings;
             updateShowBookings();
         }
-        System.out.println("h");
 
     }
 }
