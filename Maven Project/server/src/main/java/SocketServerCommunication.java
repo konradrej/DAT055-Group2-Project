4f964d3af4b06a48317b00ddd3e72df18e3692d6
Konrad Rej <31326411+konradrej@users.noreply.github.com>
2021-03-01 22:46:55 +0100

    Reformatted code, made style improvements and added javadoc comments.

--- Maven Project/server/src/main/java/SocketServerCommunication.java
+++ Maven Project/server/src/main/java/SocketServerCommunication.java
@@ -8,44 +8,46 @@ import java.net.Socket;
  * ConnectionHandler thread for each active connection.
  *
  * @author Konrad Rej
- * @version 0.1.0
+ * @version 2021-03-01
  */
 public enum SocketServerCommunication implements Runnable {
     INSTANCE();
 
-    /**
-     * Empty constructor.
-     */
-    SocketServerCommunication(){ }
+    SocketServerCommunication() {
+        Runtime.getRuntime().addShutdownHook(new Thread(() ->
+                listenForConnection = false));
+    }
+
+    boolean listenForConnection = true;
 
     /**
      * Retrieves singleton instance of SockerCommunication.
      *
      * @return SocketCommunication instance
      */
-    public static SocketServerCommunication getInstance(){
+    public static SocketServerCommunication getInstance() {
         return INSTANCE;
     }
 
     /**
-     *  Starts listening on port 888 for connections and
-     *  starts a new ConnectionHandler thread for each
-     *  established connection.
+     * Starts listening on port 888 for connections and
+     * starts a new ConnectionHandler thread for each
+     * established connection.
      */
     @Override
-    public void run(){
-        try(ServerSocket ss = new ServerSocket(888)) {
+    public void run() {
+        try (ServerSocket ss = new ServerSocket(888)) {
             Socket s;
 
-            // GÖR EXIT CONDITION
-            while(true){
+            while (listenForConnection) {
                 s = ss.accept();
                 System.out.println("Connection established.");
 
                 new Thread(new ConnectionHandler(s)).start();
             }
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
+            System.err.println("Could not listen for connections on port 888. Exiting...");
             System.exit(1);
         }
     }
