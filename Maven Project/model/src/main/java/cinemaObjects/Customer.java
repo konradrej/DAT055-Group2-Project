8200cc7c0e5900285959b8b2fa4d28fe77054b95
Erik Kieu <erik.kieu@gmail.com>
2021-03-02 14:41:34 +0100

    Added javadoc for the classes, Booking, Cinema, CinemaDate, Customer, Movie, Seat, Show, Row and Theater

--- Maven Project/model/src/main/java/cinemaObjects/Customer.java
+++ Maven Project/model/src/main/java/cinemaObjects/Customer.java
@@ -12,88 +12,83 @@ import java.io.Serializable;
  */
 public class Customer implements Serializable {
 
-	private static final long serialVersionUID = 6060898922086321725L;
-	private String name;
-	private String phoneNumber;
-	private String ssn;
+    private static final long serialVersionUID = 6060898922086321725L;
+    private String name;
+    private String phoneNumber;
+    private String ssn;
 
-	/**
-	 * Empty Constructor for initializing an empty customer instance
-	 */
-	public Customer(){
+    /**
+     * Empty Constructor for initializing an empty customer instance
+     */
+    public Customer() {
 
-	}
+    }
 
-	/**
-	 * Constructor for initializing the cinemaObjects.Customer instance
-	 * 
-	 * @param name			the name of the customer
-	 * @param phoneNumber	the phone number of the customer
-	 * @param SSN 			the social security number of the customer
-	 */
-	public Customer(String name, String phoneNumber, String SSN)
-	{
-		this.name = name;
-		this.phoneNumber = phoneNumber;
-		this.ssn = SSN;
-	}
-	
-	/**
-	 * Method for getting the name of the customer
-	 * 
-	 * @return		returns the name of the customer
-	 */
-	public String getName(){
-		return this.name;
-	}
-	
-	/**
-	 * Method for getting the phone number of the customer
-	 * 
-	 * @return		returns the phone number of the customer
-	 */
-	public String getPhoneNumber()
-	{
-		return this.phoneNumber;
-	}
-	
-	/**
-	 * Method for getting the social security number of the customer
-	 * 
-	 * @return		returns the social security number of the customer
-	 */
-	public String getSSN()
-	{
-		return this.ssn;
-	}
-	
-	/**
-	 * Method for setting the name of the customer
-	 * 
-	 * @param name	the name of the customer
-	 */
-	public void setName(String name){
-		this.name = name;
-	}
-	
-	/**
-	 * Method for setting the phone number of the customer
-	 * 
-	 * @param phoneNumber		the phone number of the customer
-	 */
-	public void setPhoneNumber(String phoneNumber)
-	{
-		this.phoneNumber = phoneNumber;
-	}
-	
-	/**
-	 * Method for setting the social security number of the customer
-	 * 
-	 * @param SSN		the social security number of the customer
-	 */
-	public void setSSN(String SSN)
-	{
-		this.ssn = SSN;
-	}
+    /**
+     * Constructor for initializing the cinemaObjects.Customer instance
+     *
+     * @param name        the name of the customer
+     * @param phoneNumber the phone number of the customer
+     * @param SSN         the social security number of the customer
+     */
+    public Customer(String name, String phoneNumber, String SSN) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.ssn = SSN;
+    }
+
+    /**
+     * Method for getting the name of the customer
+     *
+     * @return returns the name of the customer
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     * Method for getting the phone number of the customer
+     *
+     * @return returns the phone number of the customer
+     */
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    /**
+     * Method for getting the social security number of the customer
+     *
+     * @return returns the social security number of the customer
+     */
+    public String getSSN() {
+        return this.ssn;
+    }
+
+    /**
+     * Method for setting the name of the customer
+     *
+     * @param name the name of the customer
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Method for setting the phone number of the customer
+     *
+     * @param phoneNumber the phone number of the customer
+     */
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    /**
+     * Method for setting the social security number of the customer
+     *
+     * @param SSN the social security number of the customer
+     */
+    public void setSSN(String SSN) {
+        this.ssn = SSN;
+    }
 
 }
--- Maven Project/model/src/main/java/cinemaObjects/Show.java
+++ Maven Project/model/src/main/java/cinemaObjects/Show.java
@@ -13,189 +13,172 @@ import java.util.*;
  */
 public class Show implements Serializable {
 
-	private static final long serialVersionUID = -9177195168561009276L;
-	private final String uniqueID;
-	private final Movie movie;
-	private final CinemaDate dayAndTime;
-	private final Cinema cinema;
-	private final Theater theater;
-	private Boolean cancelled;
-	
-	private final List<Row> rows;
-	
-	/**
-	 * Constructor for initializing the show instance with corresponding parameter values
-	 * 
-	 * @param movie			the movie that the show will play
-	 * @param dayAndTime	the day and time of the show
-	 * @param cinema		what cinema the show will be played at
-	 * @param theater		what theater the show will be played at
-	 */
-	public Show(Movie movie, CinemaDate dayAndTime, Cinema cinema, Theater theater)
-	{
-		this.movie = movie;
-		this.dayAndTime = dayAndTime;
-		this.cinema = cinema;
-		this.theater = theater;
-		this.cancelled = false;
-		this.rows = this.theater.getCollectionOfRows();
-		this.uniqueID = UUID.randomUUID().toString();
-	}
+    private static final long serialVersionUID = -9177195168561009276L;
+    private final String uniqueID;
+    private final Movie movie;
+    private final CinemaDate dayAndTime;
+    private final Cinema cinema;
+    private final Theater theater;
+    private Boolean cancelled;
 
-	/**
-	 * Method for getting the unique ID of the show itself
-	 *
-	 * @return returns the unique ID assigned to this specific show
-	 */
-	public String getUniqueID (){
-		return this.uniqueID;
-	}
-	
-	/**
-	 * Method for getting the movie that will be played
-	 * 
-	 * @return	returns the movie instance variable of this object
-	 */
-	public Movie getMovie()
-	{
-		return this.movie;
-	}
-	
-	/**
-	 * Method for getting the day and time at which the show will take place
-	 *
-	 * @return	returns the day_and_time instance variable of this object
-	 */
-	public CinemaDate getShowDateAndTime()
-	{
-		return this.dayAndTime;
-	}
-	
-	/**
-	 * Method for getting the cinema at which the show will be played
-	 * 
-	 * @return	returns the cinema instance variable of this object
-	 */
-	public Cinema getCinema()
-	{
-		return this.cinema;
-	}
-	
-	/**
-	 * Method for getting the theater at which the show will be played
-	 * 
-	 * @return	returns the theater instance variable of this object
-	 */
-	public Theater getTheater()
-	{
-		return this.theater;
-	}
-	
-	/**
-	 * Method for getting this show object
-	 * 
-	 * @return	returns this show object
-	 */
-	public Show getShow()
-	{
-		return this;
-	}
-	
-	/**
-	 * Method for finding all seats in the theater that the show will be held in
-	 * 
-	 * @return 				returns a collection of Seat, containing all seats in the theater
-	 */
-	public ArrayList<Seat> getAllSeats()
-	{
-		ArrayList<Seat> allSeats = new ArrayList<>();
-		
-		for(Row r : this.rows)
-		{
-			allSeats.addAll(r.getAllSeats());
-		}
-		
-		return allSeats;
-	}
-	
-	/**
-	 * Method for finding all available seats in the theater that the show will be held in
-	 * 
-	 * @return 				returns a collection of Seat, containing all available seats
-	 */
-	public ArrayList<Seat> getAllAvailableSeats()
-	{
-		ArrayList<Seat> allAvailableSeats = new ArrayList<>();
-		
-		for(Row r : this.rows)
-		{
-			allAvailableSeats.addAll(r.getAllAvailableSeats());
-		}
-		
-		return allAvailableSeats;
-	}
-	
-	/**
-	 * Method for finding numOfSeats available seats in the theater that the show will be held in
-	 * 
-	 * @param numOfSeats	the number of seats to find	
-	 * @return 				returns a collection of Seat, containing numOfSeats available seats
-	 */
-	public ArrayList<Seat> getAvailableSeats(int numOfSeats)
-	{
-		ArrayList<Seat> availableSeats = new ArrayList <> ();
-		
-		for(Row r : this.rows)
-		{
-			availableSeats = r.getAvailableSeats(numOfSeats);
-			
-			if(availableSeats.size() != 0)
-			{
-				return availableSeats;
-			}
-		}
-		
-		return availableSeats;
-	}
-	
-	/**
-	 * Method for finding numOfSeats available seats adjacent to each other
-	 * 
-	 * @param numOfSeats	the number of seats to find, adjacent to each other
-	 * @return 				returns a collection of Seat, containing numOfSeats adjacent available seats
-	 */
-	public ArrayList<Seat> getAdjacentAvailableSeats(int numOfSeats)
-	{
-		ArrayList<Seat> availableAdjacentSeats = new ArrayList <> ();
-		
-		for(Row r : this.rows)
-		{
-			availableAdjacentSeats = r.getAdjacentAvailableSeats(numOfSeats);
-			
-			if(availableAdjacentSeats.size() != 0)
-			{
-				return availableAdjacentSeats;
-			}
-		}
-		
-		return availableAdjacentSeats;
-	}
-	
-	/**
-	 * Method for turning the status of the show to cancelled
-	 */
-	public void cancelShow()
-	{
-		this.cancelled = true;
-	}
+    private final List<Row> rows;
 
-	/**
-	 * Method to get the Movie , Cinema, Theater and time of the show
-	 *
-	 * @return a string representing the show
-	 */
-	public String toString(){
-		return "Movie: " + this.movie.getTitle() + "\nDate and time: " + this.dayAndTime.toString() +
-				"\nCinema: " + this.cinema.getCinemaName() + "\nTheater Number: " + theater.getTheaterNumber();
-	}
+    /**
+     * Constructor for initializing the show instance with corresponding parameter values
+     *
+     * @param movie      the movie that the show will play
+     * @param dayAndTime the day and time of the show
+     * @param cinema     what cinema the show will be played at
+     * @param theater    what theater the show will be played at
+     */
+    public Show(Movie movie, CinemaDate dayAndTime, Cinema cinema, Theater theater) {
+        this.movie = movie;
+        this.dayAndTime = dayAndTime;
+        this.cinema = cinema;
+        this.theater = theater;
+        this.cancelled = false;
+        this.rows = this.theater.getCollectionOfRows();
+        this.uniqueID = UUID.randomUUID().toString();
+    }
+
+    /**
+     * Method for getting the unique ID of the show itself
+     *
+     * @return returns the unique ID assigned to this specific show
+     */
+    public String getUniqueID() {
+        return this.uniqueID;
+    }
+
+    /**
+     * Method for getting the movie that will be played
+     *
+     * @return returns the movie instance variable of this object
+     */
+    public Movie getMovie() {
+        return this.movie;
+    }
+
+    /**
+     * Method for getting the day and time at which the show will take place
+     *
+     * @return returns the day_and_time instance variable of this object
+     */
+    public CinemaDate getShowDateAndTime() {
+        return this.dayAndTime;
+    }
+
+    /**
+     * Method for getting the cinema at which the show will be played
+     *
+     * @return returns the cinema instance variable of this object
+     */
+    public Cinema getCinema() {
+        return this.cinema;
+    }
+
+    /**
+     * Method for getting the theater at which the show will be played
+     *
+     * @return returns the theater instance variable of this object
+     */
+    public Theater getTheater() {
+        return this.theater;
+    }
+
+    /**
+     * Method for getting this show object
+     *
+     * @return returns this show object
+     */
+    public Show getShow() {
+        return this;
+    }
+
+    /**
+     * Method for finding all seats in the theater that the show will be held in
+     *
+     * @return returns a collection of Seat, containing all seats in the theater
+     */
+    public ArrayList<Seat> getAllSeats() {
+        ArrayList<Seat> allSeats = new ArrayList<>();
+
+        for (Row r : this.rows) {
+            allSeats.addAll(r.getAllSeats());
+        }
+
+        return allSeats;
+    }
+
+    /**
+     * Method for finding all available seats in the theater that the show will be held in
+     *
+     * @return returns a collection of Seat, containing all available seats
+     */
+    public ArrayList<Seat> getAllAvailableSeats() {
+        ArrayList<Seat> allAvailableSeats = new ArrayList<>();
+
+        for (Row r : this.rows) {
+            allAvailableSeats.addAll(r.getAllAvailableSeats());
+        }
+
+        return allAvailableSeats;
+    }
+
+    /**
+     * Method for finding numOfSeats available seats in the theater that the show will be held in
+     *
+     * @param numOfSeats the number of seats to find
+     * @return returns a collection of Seat, containing numOfSeats available seats
+     */
+    public ArrayList<Seat> getAvailableSeats(int numOfSeats) {
+        ArrayList<Seat> availableSeats = new ArrayList<>();
+
+        for (Row r : this.rows) {
+            availableSeats = r.getAvailableSeats(numOfSeats);
+
+            if (availableSeats.size() != 0) {
+                return availableSeats;
+            }
+        }
+
+        return availableSeats;
+    }
+
+    /**
+     * Method for finding numOfSeats available seats adjacent to each other
+     *
+     * @param numOfSeats the number of seats to find, adjacent to each other
+     * @return returns a collection of Seat, containing numOfSeats adjacent available seats
+     */
+    public ArrayList<Seat> getAdjacentAvailableSeats(int numOfSeats) {
+        ArrayList<Seat> availableAdjacentSeats = new ArrayList<>();
+
+        for (Row r : this.rows) {
+            availableAdjacentSeats = r.getAdjacentAvailableSeats(numOfSeats);
+
+            if (availableAdjacentSeats.size() != 0) {
+                return availableAdjacentSeats;
+            }
+        }
+
+        return availableAdjacentSeats;
+    }
+
+    /**
+     * Method for turning the status of the show to cancelled
+     */
+    public void cancelShow() {
+        this.cancelled = true;
+    }
+
+    /**
+     * Method to get the Movie , Cinema, Theater and time of the show
+     *
+     * @return a string representing the show
+     */
+    public String toString() {
+        return "Movie: " + this.movie.getTitle() + "\nDate and time: " + this.dayAndTime.toString() +
+                "\nCinema: " + this.cinema.getCinemaName() + "\nTheater Number: " + theater.getTheaterNumber();
+    }
 }
